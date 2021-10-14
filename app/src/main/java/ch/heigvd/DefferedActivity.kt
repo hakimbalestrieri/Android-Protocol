4d2a0dbedbe315ec74956a5ba9e4c8ead3bcfdb7
hakimbalestrieri <hakimbalestrieri@gmail.com>
2021-10-14 20:45:03 +0200

    gui config

---
+++ app/src/main/java/ch/heigvd/DefferedActivity.kt
@@ -0,0 +1,11 @@
+package ch.heigvd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DefferedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deffered)
+    }
+}
