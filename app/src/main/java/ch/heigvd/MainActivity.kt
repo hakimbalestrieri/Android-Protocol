900fe59c9257f52a06727b75584a12f941ea8bb1
hakimbalestrieri <hakimbalestrieri@gmail.com>
2021-10-14 21:03:39 +0200

    14.10

--- app/src/main/java/ch/heigvd/MainActivity.kt
+++ app/src/main/java/ch/heigvd/MainActivity.kt
@@ -1,11 +1,25 @@
 package ch.heigvd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ch.heigvd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnAsynchronousActivity.setOnClickListener {
+            startActivity(
+                Intent(this, AsynchronousActivity::class.java)
+            )
+        }
+
     }
+
+
 }
