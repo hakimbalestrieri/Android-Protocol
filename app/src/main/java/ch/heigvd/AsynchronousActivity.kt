45e4ed4a529ac1d781eda662286e9c70c2209e3e
Alexis Allemann <alexis.allemann@gmail.com>
2021-11-04 21:06:09 +0100

    Réalisation de l'asynchrone

--- app/src/main/java/ch/heigvd/AsynchronousActivity.kt
+++ app/src/main/java/ch/heigvd/AsynchronousActivity.kt
@@ -1,15 +1,35 @@
 package ch.heigvd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ch.heigvd.databinding.ActivityAsynchronousBinding
+import ch.heigvd.iict.sym.lab.comm.CommunicationEventListener
 
 /**
  * Activity in which is realized the point about the asynchronous transmissions
  * @author Allemann, Balestrieri, Gomes
  */
 class AsynchronousActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAsynchronousBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_asynchronous)
+
+        // Binding components
+        binding = ActivityAsynchronousBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Handle response of SymComManager and update UI
+        val mcm = SymComManager(object : CommunicationEventListener {
+            override fun handleServerResponse(response: String) {
+                binding.txtResult.text = response
+            }
+        })
+
+        // Adding listeners on buttons to navigate through activities
+        binding.btnSend.setOnClickListener {
+            mcm.sendRequest(getString(R.string.api_txt), binding.tbxText.text.toString())
+        }
     }
 }
--- app/src/main/java/ch/heigvd/SymComManager.kt
+++ app/src/main/java/ch/heigvd/SymComManager.kt
@@ -1,12 +1,17 @@
 package ch.heigvd
 
+import android.os.Handler
+import android.os.Looper
 import ch.heigvd.iict.sym.lab.comm.CommunicationEventListener
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
 
 /**
  * Asynchronous transmission service
  * @author Allemann, Balestrieri, Gomes
  */
-class SymComManager() {
+class SymComManager(var communicationEventListener: CommunicationEventListener) {
 
     /**
      * Send request text to the server designated by url
@@ -14,14 +19,29 @@ class SymComManager() {
      * @param request text to send
      */
     fun sendRequest(url: String, request: String) {
-        //TODO implement
-    }
-
-    /**
-     * Define a listener that will be invoked when the response reaches the client
-     * @param l the listener to invoke
-     */
-    fun setCommunicationEventListener(l: CommunicationEventListener?) {
-        // TODO implement
+        val handler = Handler(Looper.getMainLooper())
+        Thread {
+            handler.post(Runnable {
+                val connection =
+                    URL(url).openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.doInput = true
+                connection.doOutput = true
+                connection.setRequestProperty("Content-Type", "text/plain")
+                val os: OutputStream = connection.outputStream
+                val writer = BufferedWriter(OutputStreamWriter(os, "UTF-8"))
+                writer.write(request)
+                writer.flush()
+                writer.close()
+                os.close()
+                val responseCode: Int = connection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val inputStream = DataInputStream(connection.inputStream)
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val output = reader.readLine()
+                    communicationEventListener.handleServerResponse(output)
+                }
+            })
+        }
     }
 }
